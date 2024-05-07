b2ace6894c7b85d3c8f3dd054405051b7c2ce3d3
RayanSp <rayan.spall@hva.nl>
2024-05-07 13:05:24 +0200

    :fire: created header
    created header (desktop version)

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -3,8 +3,62 @@
 
 <!-- About Section -->
 <section id="about">
-t
+
+  <article class="intro">
+    <h1>Hi, I'm Rayan Spall</h1>
+    <h2>Frontend Developer</h2>
+    <button class="btn-contact">Contact Me</button>
+    <button class="btn-work" href="#">My Work</button>
+  </article>
+
 </section>
 
 <style>
+
+.intro {
+  background: var(--c-container);
+  margin: 2rem;
+  border-radius: 15px;
+  padding: 2rem;
+  color: var(--c-white);
+}
+
+h1 {
+  font-weight: lighter;
+  font-size: 25px;
+}
+
+h2 {
+  color: var(--c-lightblue);
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+}
+
+.btn-contact {
+    padding: .5rem;
+		background-color: var(--c-lightblue);
+		color: var(--c-white);
+		border: 2px solid var(--c-lightblue);
+		height: 2.5rem;
+		border-radius: 12px;
+		cursor: pointer;
+		transition: 0.2s ease-in-out;
+		font-size: 16px; 
+    text-decoration: none;
+}
+
+.btn-work {
+    padding: .5rem;
+		background-color: var(--c-lightblue);
+		color: var(--c-white);
+		border: 2px solid var(--c-lightblue);
+		margin-left: 1rem;
+		height: 2.5rem;
+		border-radius: 12px;
+		cursor: pointer;
+		transition: 0.2s ease-in-out;
+		font-size: 16px; 
+    text-decoration: none;
+}
+
 </style>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,9 +3,46 @@
 </script>
 
 <header>
-
+    <nav>
+        <label for="" class="logo">Rayan</label>
+        <ul>
+            <li a href-="#">About</li>
+            <li a href-="#">Skills</li>
+            <li a href-="#">My Work</li>
+            <li a href-="#">Contact</li>
+        </ul>
+    </nav>
 </header>
 
 <style>
-    
+
+    nav {
+        background: var(--c-container);
+        color: white;
+        margin: 2rem;
+        border-radius: 15px;
+    }
+
+    label.logo {
+        font-size: 35px;
+        line-height: 80px;
+        padding-left: 2rem;
+        color: var(--c-lightblue);
+        font-weight: bolder;
+    }
+
+    nav ul {
+        float: right;
+        list-style: none;
+        margin-right: 2rem;
+    }
+
+    nav ul li {
+        display: inline-block;
+        line-height: 80px;
+        margin: 0 30px;
+        font-weight: bold;
+        font-size: 20px;
+    }
+
 </style>
