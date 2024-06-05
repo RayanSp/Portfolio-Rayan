c3837aa7dd015e5d46d898ff515df771fcb8fe21
RayanSp <rayan.spall@hva.nl>
2024-06-05 10:44:42 +0200

    vercel fix 5?🪰
    .

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -26,7 +26,6 @@
  * ```
  */
 declare module '$env/static/private' {
-	export const npm_config_legacy_peer_deps: string;
 	export const TERM_PROGRAM: string;
 	export const NODE: string;
 	export const INIT_CWD: string;
@@ -120,7 +119,6 @@ declare module '$env/static/public' {
  */
 declare module '$env/dynamic/private' {
 	export const env: {
-		npm_config_legacy_peer_deps: string;
 		TERM_PROGRAM: string;
 		NODE: string;
 		INIT_CWD: string;
